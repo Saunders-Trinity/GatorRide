46e3ac0d6a3662dd0b777e38ed3bb29d36153925
ktran-33 <kartran309@gmail.com>
2025-12-02 12:14:08 -0500

    created stuff for backend connections to the profile, WIP report amnager

--- frontend/src/pages/reportManager/reportManager.jsx
+++ frontend/src/pages/reportManager/reportManager.jsx
@@ -1,7 +1,30 @@
 //import Navbar from "../../components/Navbar/Navbar";
 import "./reportManager.css";
+import React, {useEffect, useState} from "react";
+import ReportCard from "../components/ReportCard";
 
 const ReportManager = () => {
+    const [reports, setReports] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=> {
+        const fetchReports = async () => {
+            try{
+                const result = await fetch("http://localhost:8000/api/reports");
+                const data = await result.json();
+
+                if (result.ok){
+                    setReports(Array.isArray(data) ? data:[]);
+                }
+                else{
+                    setError(data.error || "Failed to fetch reports");
+                }
+            }
+            catch (err){
+                console.error("Fetch reports error: ", err);
+                setError()
+            }
+        }
+    })
 
     return(
         <div>
